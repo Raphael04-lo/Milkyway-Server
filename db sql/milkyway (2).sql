c864ec097ee7ffb4942c9bd69ce54d62cab35de5
Raphael Losko <losko.r04@htlwienwest.at>
2023-02-16 08:22:03 +0100

    sql datei verbessert

--- db sql/milkyway (2).sql
+++ db sql/milkyway (2).sql
@@ -21,6 +21,23 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Roles
+--
+
+
+CREATE ROLE m_admin;
+ALTER ROLE m_admin WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
+CREATE ROLE milk_admin;
+ALTER ROLE milk_admin WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'SCRAM-SHA-256$4096:N7kcvpvTFgeyj8nZggfOfg==$iPT5yOZxuZxql5m1OSnyvspLAdaVH3X0nVUGPYBO/EA=:pFma4/4SM27Uy88KXOczF54k477bB9vdTudLP+twFoE=';
+
+
+--
+-- Role memberships
+--
+
+GRANT m_admin TO milk_admin GRANTED BY postgres;
+
+--
 -- Name: article; Type: TABLE; Schema: public; Owner: m_admin
 --
 
@@ -29,7 +46,7 @@ CREATE TABLE public.article (
     title character varying(40) NOT NULL,
     summary text NOT NULL,
     description text NOT NULL,
-    i_id integer
+    img character varying(50)
 );
 
 
@@ -70,41 +87,6 @@ CREATE TABLE public.has (
 ALTER TABLE public.has OWNER TO m_admin;
 
 --
--- Name: info; Type: TABLE; Schema: public; Owner: m_admin
---
-
-CREATE TABLE public.info (
-    i_id integer NOT NULL,
-    img character varying(50) NOT NULL,
-    url character varying(50) NOT NULL
-);
-
-
-ALTER TABLE public.info OWNER TO m_admin;
-
---
--- Name: info_i_id_seq; Type: SEQUENCE; Schema: public; Owner: m_admin
---
-
-CREATE SEQUENCE public.info_i_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.info_i_id_seq OWNER TO m_admin;
-
---
--- Name: info_i_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: m_admin
---
-
-ALTER SEQUENCE public.info_i_id_seq OWNED BY public.info.i_id;
-
-
---
 -- Name: merchendise; Type: TABLE; Schema: public; Owner: m_admin
 --
 
@@ -113,7 +95,7 @@ CREATE TABLE public.merchendise (
     name character varying(40) NOT NULL,
     price numeric(6,2) NOT NULL,
     description text NOT NULL,
-    i_id integer
+    img character varying(50)
 );
 
 
@@ -156,7 +138,7 @@ CREATE TABLE public.planet (
     price_per_day numeric(6,2) NOT NULL,
     weather character varying(30) NOT NULL,
     short_decription text NOT NULL,
-    i_id integer
+    img character varying(50)
 );
 
 
@@ -231,13 +213,6 @@ ALTER TABLE ONLY public.article ALTER COLUMN a_id SET DEFAULT nextval('public.ar
 
 
 --
--- Name: info i_id; Type: DEFAULT; Schema: public; Owner: m_admin
---
-
-ALTER TABLE ONLY public.info ALTER COLUMN i_id SET DEFAULT nextval('public.info_i_id_seq'::regclass);
-
-
---
 -- Name: merchendise m_id; Type: DEFAULT; Schema: public; Owner: m_admin
 --
 
@@ -262,7 +237,9 @@ ALTER TABLE ONLY public.routes ALTER COLUMN r_id SET DEFAULT nextval('public.rou
 -- Data for Name: article; Type: TABLE DATA; Schema: public; Owner: m_admin
 --
 
-COPY public.article (a_id, title, summary, description, i_id) FROM stdin;
+COPY public.article (a_id, title, summary, description, img) FROM stdin;
+1	MyWy-113 ti	The new MyWy-113 ti makes space-trips into space naps!\r\n\r\n	\r\n\r\nFaster, comfier and cheaper. Our new night-jet class rocket offers way more than just a new design and more pillows (and blankets too!). An engine stronger than the atmosphere a red giant bears. Spacier than one of our mid-class hotels and better food served than you can ever dream of at home. And yes we have classified mommy star cooks for you on board. Starting with our new spring season, MyWy-113 ti will journey across the bright and starry universe we know together with you and our amazing team. Not me though.\r\n\r\nNot satisfied by your own apart-style suite, free room service and our on board pool, including a bar? No problem. With MyWy-113 ti we introduce our first and utmost greatest full travel experience. Offering our own, ever changing and colorful shopping center and amusement  establishments. Spas, theaters, clubs, restaurants and planet local shops. Name it all and we will deliver.\r\n\r\nNot enough? Well, then I am glad to announce that you can try out all of this for free during our “No sleep sleepover” opening event. The only catch, you have to stay awake all night! Maybe even party. Hell, what kind of an offer!\r\n\r\nWant to be a part of something bigger? Join our talented on board crew or become an exclusive contractor for our “MyWy Shopping Luxray”, the first and biggest shopping wall on moving galactic ground, also called a MyWy-113 ti. Let’s become a lightray illuminating across the universe and happy smiles of our many customers.\r\n	\N
+2	Musket’sche Meltdown	Everything a Musk touches seems to melt down and turn into a hot mess (not the good one though). Recently exactly the same happened again. Not as negative as the big Twitter war a few decades ago, but hell, it was burning.\r\n	The so-called Musket’sche Meltdown, a great mistake made again by none other than X Æ A-12 Jr. IV Musk led not only to a dozen of social media publicity but also to the destruction of the Elon factory.\r\n\r\nAnd what went wrong this time? Well, the cloning industry has been making trouble for the past decades already, especially if handling extraordinary people and celebrities. And I am sure Musk’s DNA can be called “special” with confidence. Allegedly, following the statements of locals and employees, the meme input into the chambers of the Musks were not regulated strictly enough, leading to an overdose of vitamin-reddit and vitamin-4chan. This allegedly led to an I-have-to-destroy-a-company rush deep rooted in the cells of the clones and their instinctively reaction: Throwing themselves against the glass walls and freeing even more Musks. The chain reaction quickly took over and after they painfully realized that Twitter wasn’t there to be destroyed again, they decided to end their endless suffering cycle by destroying the facility and joining the traditions of the famous Sonnentempler.\r\n\r\nBut not everything is bad. X Æ A-12 Jr. IV Musk decided to sell the tears of the Elon Clones to the public after collecting more than half a lake filling of fluid. X Æ A-12 Jr. IV Musk also exclaims that the tears of his ancestors should be able to refresh the needed meme-dosis of even the worst patients with only half of a teaspoon per day! On the other side possible side effects include:\r\nThe desire to buy Twitter\r\nA fetish for blue checkmarks\r\nThe believe that Tesla cars are actually good\r\nand that naming their son  “X Æ A-12” is a good idea\r\n	\N
 \.
 
 
@@ -275,18 +252,10 @@ COPY public.has (p_id, r_id) FROM stdin;
 
 
 --
--- Data for Name: info; Type: TABLE DATA; Schema: public; Owner: m_admin
---
-
-COPY public.info (i_id, img, url) FROM stdin;
-\.
-
-
---
 -- Data for Name: merchendise; Type: TABLE DATA; Schema: public; Owner: m_admin
 --
 
-COPY public.merchendise (m_id, name, price, description, i_id) FROM stdin;
+COPY public.merchendise (m_id, name, price, description, img) FROM stdin;
 \.
 
 
@@ -294,9 +263,10 @@ COPY public.merchendise (m_id, name, price, description, i_id) FROM stdin;
 -- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: m_admin
 --
 
-COPY public.planet (p_id, name, description, history, yearly_vistors, radius, distance, price_per_day, weather, short_decription, i_id) FROM stdin;
+COPY public.planet (p_id, name, description, history, yearly_vistors, radius, distance, price_per_day, weather, short_decription, img) FROM stdin;
 1	Mercury	Closest to the sun, Mercury has always been able to bring the most pleasant summer vibes all around the year. From the beautiful beaches, to the nightly games of the blazing flames this, fitting to its Patreon, Mercury, Metropol only blossoms economically. “Communication Dom Mercury”, the founders and Sponsors of this harmonic holiday resort, have put a lot of love and hard work into the latest projects and attractions for you to enjoy.\r\n\r\nOn the search for the perfect trip to soothe your nerves and strengthen your heart?\r\nVisit “Translucent Heaven”, a small holiday resort located at the edge of the beautiful “Seryth Falls”, a heavenly clean and soul reflection waterfall at the heart of our artificial rainforest. No wonder stars like Sasha Brady and his wonderful wife Ciera spent their most important night together right here!\r\n\r\nToo far away from the action for you? Just stay at one of the many hotels in our regional “Rapolla” campus & enjoy the beautiful messy business and market life of the large city. Museums, spars and various unique shops are waiting for you! And who knows, maybe even one of the rare student places or great paying job opportunities will fall under your lucky hands.\r\n	Surprisingly enough, Mercury has a rich, yet less well-known history of agriculture and viticulture. Many small farmers have been known for their great quality and plentiful experience in and on the field from the 22th century onwards. Wines like “Blue Blessing” or “Mercur’s” are not only one of the best but also more than affordable with our “Wine Club Membership”, that you can easily try out for free on your first trip to this astonishing planet.	450000	2439.70	0.40	420.00	Hot/Sunny	Just like earth’s romantic little Moon, only a bit hotter & without the unknown man looking down on you.	\N
 2	Earth	An all time classic. Earth offers a variety of culture, in depth war history and many fan-fictions!\r\n\r\nOur Recommendations:\r\n\r\nHalloween Trip through the wasteland of the US: Spooky Scary Skeletons might not be real, but the scary, blood soaked gun laws & bittersweet diabetes giving treats definitely are. Gaze upon the high voltage gobbling sparkling light games & the junky messy street food, while you run from house to house, begging for treats and sometimes the mercy of some Karens.\r\n\r\nGet Freaky: Convention Time - Love to talk, meet some freaky freaks & dress up? Visit one of the many conventions on earth. You like video games? Boom! Games Com. We got you! Love animals way too much?  Midwest FurFest. Enjoy some freaky mangas and more? Anime Con! Perfect! So enough for everyone, well, everyone with a passion at least!\r\n	There is a reason we reference time from earth’s history, because oh boy is this a long and interesting one. And also one we all once grew up with. But don’t you worry, we are not gonna talk about all of it here. So look it up yourself. The only thing we're gonna mention is that a lot of great food, local cultures and morals clash here everyday. Creating a great mix of the most opposite and colorful humans.\r\n	12000000	6371.00	1.00	420.00	moody  (just like you)	Quite down to earth (get it?). Anyway, enjoy the great variety offered by the dust-dry desserts, the lawful good working conditions and the light reflecting smog all around the cities.\r\n	\N
+3	Mars	A great and cheap offer for everyone who wants to be not too fancy but also wants to have some freedom again, the red planet - or as I like to call it, the orange-dust dooming ball in front of my 25qm naturally not air-conditioned apartment called prison cell! - is a great place to let some dust loose. And that even without having to handle the sticky sweat disadvantages of doing so. So get your suncream and jackets ready. It’s about to drop below zero, baby!\r\n\r\nBelow zero, not the spanish action thriller with a measly audience score of 54%, is our most famous part of Mars history, galactic slavery! Just kidding, we won’t talk about Musk's below zero policy, because that’s already far gone. The only below zero we offer is measured in celsius and the free food we offer of course. For simplicity we're gonna measure that in celsius too.\r\n\r\nDespite being nearly as cheap as my hourly wage, the red giant has a lot to offer. Ever wanted to ride a sand buggy through the endless cold of a far away planet? Now you can & you can do so with style (and a small fee).\r\n\r\nWar is your hobby? Luckily not only the god Mars, but also the planet, offer some great war grounds. Okay, maybe we can’t offer you real weapons while you are on them, but paintball sounds like fun too, am I right, mate?\r\n\r\nYou aren’t a paintball enjoying softy? Well, I have the perfect deal for you too! Our premium deluxe, ultra rare puppy restaurant (with pickup service). Oh and therapy sessions if the belt of your father still flashes your dreams.\r\n\r\nDid I piss you off? Luckily my office is right at Iodgevackgasse 0 Door 3. Just knock at the door and ask for a good time, maybe we can figure your anger out like that ;)\r\n	I really don’t like this part, especially because of the many books I have to read for this. So I won’t do it…\r\nOkay, I will. The penny for every hour I work is worth it after all.\r\nMars - The Red Planet - was always a big interest for our unknowing ancestors, especially for Musk and not mustache enjoyers. With the potential to replace earth one day and therefore leave all our self made problems behind (and this time not behind our grave) a lot of fuss was made. Despite me calling it a fuss, there was progress made, just not enough, especially not after the great Twitter war 2025. And after leaving a few hundred fanboys stranded on Mars the story pretty much ends with a few ruins and rotten bodies. Oh and a quite lit native folk that still exists. They make great corndogs, so just you know.\r\n	2000000	3.39	1.50	50.00	sunny init	Basic, basic & basic! Musk’s holy self-inserted little part of human history. Still, I like it. The food and sand buggies are funny. So take a shot (not literally) & discover the red planet.	\N
 \.
 
 
@@ -312,14 +282,7 @@ COPY public.routes (r_id, name, price, distance, class, person, description) FRO
 -- Name: article_a_id_seq; Type: SEQUENCE SET; Schema: public; Owner: m_admin
 --
 
-SELECT pg_catalog.setval('public.article_a_id_seq', 1, false);
-
-
---
--- Name: info_i_id_seq; Type: SEQUENCE SET; Schema: public; Owner: m_admin
---
-
-SELECT pg_catalog.setval('public.info_i_id_seq', 1, false);
+SELECT pg_catalog.setval('public.article_a_id_seq', 2, true);
 
 
 --
@@ -333,7 +296,7 @@ SELECT pg_catalog.setval('public.merchendise_m_id_seq', 1, false);
 -- Name: planet_p_id_seq; Type: SEQUENCE SET; Schema: public; Owner: m_admin
 --
 
-SELECT pg_catalog.setval('public.planet_p_id_seq', 2, true);
+SELECT pg_catalog.setval('public.planet_p_id_seq', 3, true);
 
 
 --
@@ -357,14 +320,6 @@ ALTER TABLE ONLY public.article
 
 ALTER TABLE ONLY public.has
     ADD CONSTRAINT has_pkey PRIMARY KEY (p_id, r_id);
-
-
---
--- Name: info info_pkey; Type: CONSTRAINT; Schema: public; Owner: m_admin
---
-
-ALTER TABLE ONLY public.info
-    ADD CONSTRAINT info_pkey PRIMARY KEY (i_id);
 
 
 --
@@ -392,59 +347,11 @@ ALTER TABLE ONLY public.routes
 
 
 --
--- Name: article article_i_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: m_admin
---
-
-ALTER TABLE ONLY public.article
-    ADD CONSTRAINT article_i_id_fkey FOREIGN KEY (i_id) REFERENCES public.info(i_id);
-
-
---
--- Name: article fk_info_article; Type: FK CONSTRAINT; Schema: public; Owner: m_admin
---
-
-ALTER TABLE ONLY public.article
-    ADD CONSTRAINT fk_info_article FOREIGN KEY (i_id) REFERENCES public.info(i_id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- Name: merchendise fk_info_merchendise; Type: FK CONSTRAINT; Schema: public; Owner: m_admin
---
-
-ALTER TABLE ONLY public.merchendise
-    ADD CONSTRAINT fk_info_merchendise FOREIGN KEY (i_id) REFERENCES public.info(i_id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- Name: planet fk_info_planet; Type: FK CONSTRAINT; Schema: public; Owner: m_admin
---
-
-ALTER TABLE ONLY public.planet
-    ADD CONSTRAINT fk_info_planet FOREIGN KEY (i_id) REFERENCES public.info(i_id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
 -- Name: has fk_r_id; Type: FK CONSTRAINT; Schema: public; Owner: m_admin
 --
 
 ALTER TABLE ONLY public.has
     ADD CONSTRAINT fk_r_id FOREIGN KEY (r_id) REFERENCES public.routes(r_id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- Name: merchendise merchendise_i_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: m_admin
---
-
-ALTER TABLE ONLY public.merchendise
-    ADD CONSTRAINT merchendise_i_id_fkey FOREIGN KEY (i_id) REFERENCES public.info(i_id);
-
-
---
--- Name: planet planet_i_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: m_admin
---
-
-ALTER TABLE ONLY public.planet
-    ADD CONSTRAINT planet_i_id_fkey FOREIGN KEY (i_id) REFERENCES public.info(i_id);
 
 
 --
