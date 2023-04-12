846d51ded372343374baef50bb1a779bfad5c361
Raphael Losko <losko.r04@htlwienwest.at>
2023-04-12 13:07:11 +0200

    updated client for render

--- db/index.js
+++ db/index.js
@@ -1,7 +1,18 @@
 import pg from 'pg';
 
-const pool = new pg.Pool();
+// const pool = new pg.Pool();
 
-const query = (text, params) => pool.query(text, params);
+// const query = (text, params) => pool.query(text, params);
 
-export { query, pool };
+// export { query, pool };
+
+const client = new pg.Client({
+  user: 'admin',
+  password: 'IVBvaCzsx8O4ZA1qmzlFS5lYDoljYCeh',
+  database: 'milkyway_i3k7',
+  port: 5432,
+  host: 'dpg-cgqikau4dad5es12ji50-a.frankfurt-postgres.render.com',
+  ssl: true,
+});
+
+export { client };
--- models/milkyway.js
+++ models/milkyway.js
@@ -1,14 +1,4 @@
-// import { query, pool } from '../db/index.js';
-import pg from 'pg';
-
-var client = new pg.Client({
-  user: 'admin',
-  password: 'IVBvaCzsx8O4ZA1qmzlFS5lYDoljYCeh',
-  database: 'milkyway_i3k7',
-  port: 5432,
-  host: 'dpg-cgqikau4dad5es12ji50-a.frankfurt-postgres.render.com',
-  ssl: true,
-});
+import { client } from '../db/index.js';
 
 client.connect();
 
